736449db39ea4a6d81c6d9e891467cfbad05fc72
empitsu <oneok0813@gmail.com>
2020-05-03 20:02:23 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "airbnb-base"j],
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: "module",
+  },
+};
---
+++ src/app.js
@@ -0,0 +1,3 @@
+import { cv } from "opencv-wasm";
+console.log('opencv-webpack-sample');
+console.log({cv})
